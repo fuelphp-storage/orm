11f330da459f177922e51f4794628f54d2f76d55
Steve "uru" West <uruwolf@gmail.com>
2014-01-12 11:35:00 +0000

    Bumps copyright notice year

---
+++ src/Fuel/Orm/ModelCollection.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package   Fuel\Orm
+ * @version   2.0
+ * @author    Fuel Development Team
+ * @license   MIT License
+ * @copyright 2010 - 2014 Fuel Development Team
+ * @link      http://fuelphp.com
+ */
+
+namespace Fuel\Orm;
+
+/**
+ * Class ModelCollection
+ *
+ * @package Fuel\Orm
+ * @author  Fuel Development Team
+ * @since   2.0
+ */
+class ModelCollection
+{
+
+}
